b71a3cd00c4230a6feb22b3f74967932da86d467
Chris Rodriguez <chrisleerod@gmail.com>
2021-01-29 02:34:05 -0600

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 import Homepage from "./components/homepage/homepage";
@@ -8,10 +8,18 @@ import Freelance from "./components/freelance/freelance.js";
 import Error from "./components/404/Error.js";
 import Google from "./components/google/index.jsx";
 
+import ReactGa from 'react-ga';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
+
+  useEffect(() => {
+    ReactGa.initialize('UA-187561372-1')
+
+    ReactGa.pageview('/')
+  }, [])
   return (
     <Router>
       <ScrollToTop />
